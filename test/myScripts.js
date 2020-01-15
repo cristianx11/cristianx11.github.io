db64dad8119e83ba3e6f5362b97b15e3a02df525
cristianx11 <55456817+cristianx11@users.noreply.github.com>
2020-01-15 13:45:19 -0500

    Create myScripts.js

---
+++ test/myScripts.js
@@ -0,0 +1,44 @@
+document.write("Loop A <br/>");
+  var i;
+for(i = 0; i < 31; i++) {
+  document.write("My Loop:" + i + "<br/>");
+}
+
+document.write("Loop B <br/>");
+var f;
+for(f = 200; f > 149; f--) {
+  document.write("My Loop:" + f + "<br/>");
+}
+
+document.write("Loop C <br/>");
+var m;
+for(m = 0; m < 100; m+=2) {
+  document.write("My Loop:" + m + "<br/>");
+}
+
+document.write("Loop D <br/>");
+var l;
+for(l = 100; l < 5100; l+=100) {
+  document.write("My Loop:" + l + "<br/>");
+}
+
+document.write("Loop E <br/>");
+var c;
+for(c = 0; c < 9; c++) {
+  var ans= Math.pow(2,c);
+  document.write("The power is:" + ans);
+}
+document.write("Loop F <br/>");
+var c;
+for(c = 0; c < 9; c++) {
+  var ans= Math.sqrt(c);
+  document.write("The power is:" + ans);
+}
+document.write("Loop G <br/>");
+var ind = 0;
+while(ind < 10)
+  {
+    var ans = Math.random(ind);
+    document.write(ans + "<br/>");
+    ind++;
+  }
