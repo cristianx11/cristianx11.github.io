d5fe7247e6b3e46a9820ef1cdfecdbf4352edb47
cristianx11 <55456817+cristianx11@users.noreply.github.com>
2020-01-24 13:32:42 -0500

    Update myScripts.js

--- test/myScripts.js
+++ test/myScripts.js
@@ -42,3 +42,18 @@ while(ind < 10)
     document.write(ans + "<br/>");
     ind++;
   }
+<!DOCTYPE html>
+<html>
+  <body>
+    <h4>Where to start?   </h4>
+    <input type="text" id="start" />
+    
+    <h4>Where to end?   </h4>
+    <input type="text" id="end" />
+    
+    <h4>How much to jump? </h4>
+    <input type="text" id="jump" />
+    
+    <button onclick="myLoop()" >Loop Me!     </button>
+  </body>
+</html>
